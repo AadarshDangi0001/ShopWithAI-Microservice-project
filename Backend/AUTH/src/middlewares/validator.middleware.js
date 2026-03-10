d33c554e91d182e18daa83eb559c48b47f03f14a
Aadarsh Dangi <aadarsht0001@gmail.com>
2026-03-11 00:28:06 +0530

    nodemailer setup

--- Backend/AUTH/src/middlewares/validator.middleware.js
+++ Backend/AUTH/src/middlewares/validator.middleware.js
@@ -14,7 +14,7 @@ const registerUserValidator = [
     .isString()
     .withMessage('Username must be a string')
     .notEmpty()
-    .withMessage('Username is required')
+    .withMessage('Username is required') 
     .isLength({min:3, max:30})
     .withMessage('Username must be between 3 and 30 characters'),
 
